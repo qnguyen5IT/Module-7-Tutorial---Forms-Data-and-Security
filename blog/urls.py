5a06e06461dabb3cd9eb8cb1f1ca897b2f223f88
Quang <qnguyen5@IvyTech.edu>
2024-10-08 23:56:06 -0400

    M07 Tutorial

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('', include('blog.urls')),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
 ]
